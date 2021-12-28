31bfd47acdc69eac7e31a186831bcdb6ac1ae024
Cole Herman <cole.herman370@outlook.com>
2021-12-28 00:29:52 -0600

    Update

--- Mapping_GeoJSON/static/js/logic.js
+++ Mapping_GeoJSON/static/js/logic.js
@@ -1,22 +1,4 @@
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+let airportData = "https://github.com/coleherman370/mapping_earthquakes/blob/GeoJSON/Mapping_GeoJSON/static/json/majorAirports.json";
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
@@ -37,12 +19,12 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 streets.addTo(map);
 
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  pointToLayer: function(feature, latlng) {
-    console.log(feature);
-    return L.marker(latlng)
-    .bindPopup("<h2>" + feature.properties.city + "</h2>");
-  }
-
-}).addTo(map);
+d3.json(airportData).then(function(data) {
+  console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+    }
+    }).addTo(map);
+});
