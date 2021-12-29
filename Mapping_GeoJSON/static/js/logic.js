1d5a901c2e2e94f5d2e36dfaa5d00d91fe823c7d
Cole Herman <cole.herman370@outlook.com>
2021-12-28 20:29:58 -0600

    Update

--- Mapping_GeoJSON/static/js/logic.js
+++ Mapping_GeoJSON/static/js/logic.js
@@ -1,12 +1,4 @@
-let airportData = "https://github.com/coleherman370/mapping_earthquakes/blob/GeoJSON/Mapping_GeoJSON/static/json/majorAirports.json";
-
-// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      30.0, 30.0
-    ],
-    zoom: 2
-  });
+let airportData = "https://raw.githubusercontent.com/coleherman370/mapping_earthquakes/GeoJSON/majorAirports.json";
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -15,8 +7,30 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+  center: [
+    30.0, 30.0
+  ],
+  zoom: 2,
+  layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
@@ -28,3 +42,4 @@ d3.json(airportData).then(function(data) {
     }
     }).addTo(map);
 });
+
